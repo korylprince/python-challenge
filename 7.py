b93bbd660d50cf94e36669b6ed5f55092bf617f9
Kory Prince <korylprince@gmail.com>
2012-07-22 22:07:08 -0500

    added 7,8

--- 7.py
+++ 7.py
@@ -6,8 +6,10 @@ import Image
 data = StringIO.StringIO(urllib2.urlopen('http://www.pythonchallenge.com/pc/def/oxygen.png').read())
 pic = Image.open(data)
 codes = []
+# loop through pixels about halfway down
 for x in xrange(0,pic.size[0],7):
     pixel = pic.getpixel((x,45))
+    # make sure it's greyscale
     if pixel[0] == pixel[1] == pixel[2]:
         codes.append(pixel[0])
 print ''.join([chr(x) for x in codes])
