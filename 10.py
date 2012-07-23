cfbf78525c811fd838e038781e15de9e493de990
Kory Prince <korylprince@gmail.com>
2012-07-23 17:21:16 -0500

    added 10,11

--- 10.py
+++ 10.py
@@ -1,1 +1,32 @@
-# http://www.pythonchallenge.com/pc/return/bull.html
+# http://www.pythonchallenge.com/pc/return/bull.html, un=huge,pw=file
+
+#This counts out the previous number 1, 11 (one 1), 21 (two 1s), etc
+
+
+number = '1'
+numbers = ['1']
+while len(numbers)<31:
+    strnum = ''
+    index = 0
+    count = 0
+    prev = '0'
+    while index <= len(number):
+        if index == len(number):
+            strnum += str(count)+prev
+            break
+        if prev == '0':
+            prev = number[index]
+            count = 1
+            index += 1
+        else:
+            if prev == number[index]:
+                count += 1
+                index += 1
+            else:
+                strnum += str(count)+prev
+                prev = number[index]
+                count = 1
+                index += 1
+    numbers.append(strnum)
+    number = strnum
+print len(numbers[30])
---
+++ 11.py
@@ -0,0 +1,18 @@
+# # http://www.pythonchallenge.com/pc/return/5808.html, un=huge,pw=file 
+import urllib2,base64
+import StringIO
+import Image
+
+request = urllib2.Request("http://www.pythonchallenge.com/pc/return/cave.jpg")
+base64string = base64.encodestring('%s:%s' % ('huge', 'file')).replace('\n', '')
+request.add_header("Authorization", "Basic %s" % base64string)   
+
+data = StringIO.StringIO(urllib2.urlopen(request).read())
+pic = Image.open(data)
+
+odd = Image.new("RGB",(320,240))
+
+for x in xrange(0,640,2):
+    for y in xrange(0,480,2):
+        odd.putpixel((x/2,y/2),pic.getpixel((x,y)))
+odd.show()
