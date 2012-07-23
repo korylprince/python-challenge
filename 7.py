fd8fd586adbd3abe49ae21fd6ecde0ed1409987a
Kory Prince <korylprince@gmail.com>
2012-07-22 21:52:47 -0500

    added 7

---
+++ 7.py
@@ -0,0 +1,17 @@
+# http://www.pythonchallenge.com/pc/def/oxygen.html
+import urllib2
+import StringIO
+import Image
+
+data = StringIO.StringIO(urllib2.urlopen('http://www.pythonchallenge.com/pc/def/oxygen.png').read())
+pic = Image.open(data)
+codes = []
+for x in xrange(0,pic.size[0],7):
+    pixel = pic.getpixel((x,45))
+    if pixel[0] == pixel[1] == pixel[2]:
+        codes.append(pixel[0])
+print ''.join([chr(x) for x in codes])
+
+# add in array from pic:
+newcodes = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+print ''.join([chr(x) for x in newcodes])
